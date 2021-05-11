0183c83ef8eaed9e673d76a564ae9ba8aaa59256
Charliemur2 <charliemurciamunoz@gmail.com>
2021-05-10 20:51:55 -0500

    header finiched for all sizes. Pend. fix navbar size on desktop size

--- script.js
+++ script.js
@@ -2,14 +2,35 @@ const menuBtn = document.querySelector('.js-menu-btn');
 const navBar = document.querySelector('.js-navbar');
 let menuOpen = false;
 
+/* active item on navbar */
+let navItem = document.querySelectorAll('.js-navbar__link');
+navItem.forEach(item => {
+  item.addEventListener('click', function() {
+    navItem.forEach(navItem => navItem.classList.remove('navbar__item--active'));
+    this.classList.add('navbar__item--active');
+  });
+});
+
 menuBtn.addEventListener('click', () => {
   if (!menuOpen) {
     menuBtn.classList.add('open');
     navBar.classList.add('navbar--shown');
     menuOpen = true;
+    disableScroll();
   } else {
     menuBtn.classList.remove('open');
     navBar.classList.remove('navbar--shown');
     menuOpen = false;
+    enableScroll();
   }
 });
+
+/* disable and enable srolling */
+function disableScroll() {
+  document.body.style.overflow = 'hidden';
+  document.querySelector('html').scrollTop = window.scrollY;
+}
+
+function enableScroll() {
+  document.body.style.overflow = null;
+}
